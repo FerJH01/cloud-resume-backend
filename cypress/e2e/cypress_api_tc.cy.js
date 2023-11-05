3ad90133a4bbce49302d207c64424b1f6438f923
FerJH01 <appferjh@gmail.com>
2023-11-05 20:30:49 -0300

    adding cypress test

---
+++ cypress/e2e/cypress_api_tc.cy.js
@@ -0,0 +1,8 @@
+it("API endpoint test", () => {
+  cy.request(
+    "GET",
+    "https://fra90a3sa5.execute-api.us-east-1.amazonaws.com/PROD/"
+  ).should((response) => {
+    expect(response.status).to.eq(200);
+  });
+});
