d4350d5f9cb4161432d718c73fa4dd37e203a0ad
FerJH01 <appferjh@gmail.com>
2023-11-05 14:31:14 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import json
+import boto3
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('visitorCounterTable')
+def lambda_handler(event, context):
+    response = table.get_item(Key={
+        'visitor_id':'VisitorCount'
+    })
+    item = response["Item"]
+    views = response['Item']['visitor_counter']
+    views = views + 1
+    print(views)
+    response = table.put_item(Item={
+        'visitor_id':'VisitorCount',
+        'visitor_counter':views
+    })
+    
+    return{
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            },
+          "body": json.dumps({"Visit_Count": str(item["visitor_counter"] + 1)})
+    }
